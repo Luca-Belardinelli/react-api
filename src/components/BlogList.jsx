fdd05cfaa9dc1539c24778fe4987dc2512319c2d
Luca Belardinelli <belardinelliluca@gmail.com>
2025-02-20 17:51:57 +0100

    blogs map

---
+++ src/components/BlogList.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+
+const BlogList = () => {
+
+    // UTILIZZO USESTATE PER LA GESTIONE DEI DATA 
+
+    const [blogs, setBlogs] = useState([]);
+
+    // FUNZIONE DI GESTIONE CHIAMATA API
+
+    function fetchPosts() {
+
+        axios.get("http://localhost:3000/posts")
+
+            .then((res) =>
+
+                setBlogs(res.data)
+            )
+    }
+
+    // CHIAMATA SOLO AL PRIMO RENDERING
+    useEffect(fetchPosts, []);
+
+    return (
+        <>
+
+            {/* MAP */}
+
+            {
+                blogs.map((post) => (
+
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <img src={post.image} alt={post.title} />
+                        <p>{post.content}</p>
+                        <span>{post.tags} </span>
+                    </div>
+
+                ))
+            }
+        </>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default BlogList

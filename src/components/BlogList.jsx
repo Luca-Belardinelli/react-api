96b2417d5a65baf59ec81493eb2b59cc1824d79f
Luca Belardinelli <belardinelliluca@gmail.com>
2025-02-21 14:57:37 +0100

    new form

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -2,12 +2,60 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
+//  AGGIUNGIAMO I CAMPI VUOTI AL FORM
+const initialFormData = {
+    titolo: "",
+    content: "",
+    image: "",
+    tags: "",
+};
+
 
 const BlogList = () => {
 
     // UTILIZZO USESTATE PER LA GESTIONE DEI DATA 
 
     const [blogs, setBlogs] = useState([]);
+
+    //STATE GESTIONE INFORMAZIONI RACCOLTE DAI CAMPI DEL FORM
+
+    const [formData, setFormData] = useState(initialFormData);
+
+    // FUNZIONE GESTIONE INFO CAMPI
+    function handleFormData(e) {
+
+        // GESTIONE VALUE A SECONDA DEL TIPO DI INPUT
+        const value = e.target.type === "" ? e.target.checked : e.target.value;
+
+        //SETTA TRAMITE SETSTATE L'OGGETTO CON LE INFO PRESI DAI CAMPI DEL FORM
+        setFormData((currentFormData) => ({
+            ...currentFormData,
+            [e.target.name]: value,
+        }));
+    }
+
+
+    //FUNZIONE GESTIONE INVIO DELL'INTERO FORM
+    function handleSubmit(e) {
+        e.preventDefault();
+        setBlogs((currentBlogs) => [...currentBlogs, {
+            id:
+                currentBlogs.length === 0 ? 1 : currentBlogs[currentBlogs.length - 1].id + 1,
+            ...formData
+        }]);
+        //RESETTO IL FORM
+        setFormData(initialFormData);
+    }
+
+    // FUNZIONE CANCELLAZIONE BLOG
+    function deleteBlog(idBlog) {
+        // NUOVO ARRAY DA SOSTITUIRE ALLO STATE BLOGS
+        const updateBlogs = blogs.filter((blog) => {
+            return blog.id !== idBlog;
+        })
+        // SOSTITUZIONE
+        setBlogs(updateBlogs);
+    }
 
     // FUNZIONE DI GESTIONE CHIAMATA API
 
@@ -26,6 +74,50 @@ const BlogList = () => {
 
     return (
         <>
+            <form action="#" onSubmit={handleSubmit}>
+                {/* valore titolo blog */}
+                <input
+                    type="text"
+                    name="titolo"
+                    onChange={handleFormData}
+                    value={formData.titolo}
+                    placeholder='titolo del blog'
+                />
+
+
+                {/* valore contenuto blog */}
+                <textarea
+                    type="text"
+                    name="content"
+                    onChange={handleFormData}
+                    value={formData.contenuto}
+                    placeholder='contenuto del blog'
+                />
+
+                {/* valore autore blog */}
+                <input
+                    type="text"
+                    name="image"
+                    onChange={handleFormData}
+                    value={formData.autore}
+                    placeholder='img'
+                />
+
+
+                {/* valore categoria blog */}
+                <input
+                    type="text"
+                    name="tags"
+                    onChange={handleFormData}
+                    value={formData.categoria}
+                    placeholder='tags'
+                />
+
+                <button>Aggiungi</button>
+
+
+            </form >
+
 
             {/* MAP */}
 
